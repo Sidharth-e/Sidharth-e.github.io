0740721b5f6201b7244bba521095879f6722527d
Sidharth_E <62341185+Sidharth-e@users.noreply.github.com>
2022-09-03 20:26:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,80 @@
-// jQuery script to nav scroll to section
+// ---------Responsive-navbar-active-animation-----------
+function test() {
+	var tabsNewAnim = $("#navbarSupportedContent");
+	var selectorNewAnim = $("#navbarSupportedContent").find("li").length;
+	var activeItemNewAnim = tabsNewAnim.find(".active");
+	var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+	var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+	var itemPosNewAnimTop = activeItemNewAnim.position();
+	var itemPosNewAnimLeft = activeItemNewAnim.position();
+	$(".hori-selector").css({
+		top: itemPosNewAnimTop.top + "px",
+		left: itemPosNewAnimLeft.left + "px",
+		height: activeWidthNewAnimHeight + "px",
+		width: activeWidthNewAnimWidth + "px"
+	});
+	$("#navbarSupportedContent").on("click", "li", function (e) {
+		$("#navbarSupportedContent ul li").removeClass("active");
+		$(this).addClass("active");
+		var activeWidthNewAnimHeight = $(this).innerHeight();
+		var activeWidthNewAnimWidth = $(this).innerWidth();
+		var itemPosNewAnimTop = $(this).position();
+		var itemPosNewAnimLeft = $(this).position();
+		$(".hori-selector").css({
+			top: itemPosNewAnimTop.top + "px",
+			left: itemPosNewAnimLeft.left + "px",
+			height: activeWidthNewAnimHeight + "px",
+			width: activeWidthNewAnimWidth + "px"
+		});
+	});
+}
+$(document).ready(function () {
+	setTimeout(function () {
+		test();
+	});
+});
+$(window).on("resize", function () {
+	setTimeout(function () {
+		test();
+	}, 500);
+});
+$(".navbar-toggler").click(function () {
+	$(".navbar-collapse").slideToggle(300);
+	setTimeout(function () {
+		test();
+	});
+});
 
-$(document).ready(function(){
-      $('.nav li a').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-        && location.hostname == this.hostname) {
-          var $target = $(this.hash);
-          $target = $target.length && $target
-          || $('[name=' + this.hash.slice(1) +']');
-          if ($target.length) {
-            var targetOffset = $target.offset().top;
-            $('html,body')
-            .animate({scrollTop: targetOffset}, 1000);
-           return false;
-          }
-        }
-      });
-    });
-    
-    //jQuery script to page-scroll & animate fa-icon
-    
-    $(document).ready(function() { 
-      $('a.page-scroll').click(function() {  
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')   && location.hostname == this.hostname) {   
-          var $target = $(this.hash);   
-          $target = $target.length && $target    || $('[name=' + this.hash.slice(1) + ']');   
-          if ($target.length) {    
-            var targetOffset = $target.offset().top;    
-            $('html,body')    .animate({
-              scrollTop: targetOffset
-            }, 1000);    
-            return false;   
-          }  
-        } 
-      });
-    });
+// --------------add active class-on another-page move----------
+jQuery(document).ready(function ($) {
+	// Get current path and find target link
+	var path = window.location.pathname.split("/").pop();
+
+	// Account for home page with empty path
+	if (path == "") {
+		path = "index.html";
+	}
+
+	var target = $('#navbarSupportedContent ul li a[href="' + path + '"]');
+	// Add active class to target link
+	target.parent().addClass("active");
+});
+
+// Add active class on another page linked
+// ==========================================
+// $(window).on('load',function () {
+//     var current = location.pathname;
+//     console.log(current);
+//     $('#navbarSupportedContent ul li a').each(function(){
+//         var $this = $(this);
+//         // if the current path is like this link, make it active
+//         if($this.attr('href').indexOf(current) !== -1){
+//             $this.parent().addClass('active');
+//             $this.parents('.menu-submenu').addClass('show-dropdown');
+//             $this.parents('.menu-submenu').parent().addClass('active');
+//         }else{
+//             $this.parent().removeClass('active');
+//         }
+//     })
+// });
+
